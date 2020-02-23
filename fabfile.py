d2fcfdd2e2691b9617c12e00722d888f7f4ef2c6
zealzel <zealzel@gmail.com>
2020-02-23 16:23:05 +0800

    update

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,20 @@
 from textwrap import dedent
 from fabric import Connection, task
 from fabcore import Remote, get_connection, appends_test
+from fab_nginx import set_nginx
+from provision import package_update, download_pip, pyenv
 
 PROJECT_NAME = 'kitty_reward'
 HOME = '~'
 PROJECT_ROOT = f'{HOME}/codes'
 PROJECT_PATH = f'{PROJECT_ROOT}/{PROJECT_NAME}'
-#  FILE_RC = f'{HOME}/.bash_profile'
+FILE_RC = f'{HOME}/.bash_profile'
 
 REPO_URL = f'https://github.com/zealzel/{PROJECT_NAME}.git'
 LOCAL_USER = 'zealzel'
 
+#  MY_DOMAIN_COM = 'momomuji.xyz'
+MY_DOMAIN_COM = 'fitfabsw.club'
 PORT = 5223
 
 
@@ -34,7 +38,6 @@ def xenial(ctx):
     ctx.port = '2200'
     ctx.key = f'/Users/{LOCAL_USER}/vagrant_machines/{ctx.vagrant_box}/.vagrant/machines/default/virtualbox/private_key'
     ctx.forward_agent = True
-    ctx.filerc = f'{HOME}/.bash_profile'
     conn = get_connection(ctx)
     ctx.conn = conn
 
@@ -48,96 +51,15 @@ def linode(ctx):
     ctx.key = None
     #  ctx.key = f'/Users/{LOCAL_USER}/.ssh/id_rsa'
     #  ctx.forward_agent = False
-    ctx.filerc = f'{HOME}/.bash_profile'
     conn = get_connection(ctx)
     ctx.conn = conn
-
-
-@task
-def bashrc(ctx):
-    conn = ctx.conn
-    r = Remote(conn)
-    lines = dedent(f'''
-    if [ -f ~/.bashrc ]; then
-        source ~/.bashrc
-    fi
-    ''').strip()
-    r.appends('~/.bash_profile', lines, ctx.user)
 
 
 @task
 def provision(ctx):
     package_update(ctx)
     download_pip(ctx)
-    pyenv(ctx)
-
-
-@task
-def package_update(ctx):
-    conn = ctx.conn
-    conn.sudo('apt update')
-
-
-
-@task
-def download_pip(ctx):
-    ''' === underscore is replaced with hyphens ===
-        fab download-pip
-    '''
-    url = 'https://bootstrap.pypa.io/get-pip.py'
-    conn = ctx.conn
-    conn.run(f'curl {url} -o get-pip.py')
-    '''
-        copy pip binary into ~/.local/bin
-    '''
-    conn.run('python3 get-pip.py')
-
-
-@task
-def pyenv(ctx):
-    conn = ctx.conn
-    r = Remote(conn)
-    '''
-    Prerequisites for ubuntu
-    reference: https://github.com/pyenv/pyenv/wiki/Common-build-problems
-    '''
-    conn.sudo('apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-              libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-              xz-utils tk-dev libffi-dev liblzma-dev python-openssl git')
-
-    home = conn.run('echo $HOME').stdout.strip()
-    path = conn.run('echo $PATH').stdout.strip()
-    pyenv_root = f'{home}/.pyenv'
-
-    with conn.cd(HOME):
-        if conn.run('source ~/.bash_profile; pyenv --version', warn=True, hide=True).failed:
-            print('pyenv not installed')
-            '''
-            pyenv respository at github
-            https://github.com/pyenv/pyenv#locating-the-python-installation
-            '''
-            conn.run(f'git clone https://github.com/pyenv/pyenv.git {pyenv_root}', warn=True)
-            content = dedent(f'''
-            export PYENV_ROOT={pyenv_root}
-            export PATH={pyenv_root}/bin:$PATH
-            if command -v pyenv 1>/dev/null 2>&1; then
-              eval "$(pyenv init -)"
-            fi
-            ''').strip()
-            r.appends(ctx.filerc, content, ctx.user)
-
-        print('pyenv installed. download pyenv-virtualenv')
-        '''
-        pyenv-virtualenv respository at github
-        https://github.com/pyenv/pyenv-virtualenv
-        '''
-        conn.run('git clone https://github.com/pyenv/pyenv-virtualenv.git'
-                 f' {pyenv_root}/plugins/pyenv-virtualenv', warn=True)
-        conn.run(
-            f'source {ctx.filerc};'
-            f'pyenv install {PYTHON_VER};'
-            f'pyenv virtualenv {PYTHON_VER} {VIRTUALENV_NAME}'
-        )
+    pyenv(ctx, FILE_RC, PYTHON_VER, VIRTUALENV_NAME)
 
 
 @task
@@ -145,7 +67,7 @@ def download_py_packages(ctx):
     conn= get_connection(ctx)
     with conn.cd(PROJECT_PATH):
         conn.run(
-            f'source {ctx.filerc} &&'
+            f'source {FILE_RC} &&'
             f'pyenv activate {VIRTUALENV_NAME} &&'
             f'pip install -U pip &&'
             f'pip install -r requirements.txt'
@@ -159,10 +81,6 @@ def echo(ctx, message): # with arguments
     '''
     conn = get_connection(ctx)
     conn.run(f'echo {message}')
-
-
-def exists(file, dir):
-    return file in dir
 
 
 @task
@@ -179,7 +97,7 @@ def clone(ctx):
     with conn.cd(PROJECT_ROOT):
         ls_result = conn.run("ls").stdout
         ls_result = ls_result.split("\n")
-        if exists(PROJECT_NAME, ls_result):
+        if PROJECT_NAME in ls_result:
             print("project already exists")
             return
         conn.run(f'git clone {REPO_URL} {PROJECT_NAME}')
@@ -209,28 +127,8 @@ def deploy(ctx):
     # install nginx
     r.apt_install('nginx')
 
-    #  print("restarting the nginx...")
-    #  restart(conn)
-
-
-@task
-def set_nginx(ctx):
-    r = Remote(get_connection(ctx))
-    lines = dedent(f'''
-    server {{
-       server_name momomuji.xyz;
-       location / {{
-           proxy_pass http://localhost:5000;
-       }}
-
-    }}
-    server {{
-       listen 80;
-       server_name momomuji.xyz;
-    }}
-    ''').strip()
-    process = PROJECT_NAME
-    r.touch(f'/etc/nginx/sites-available/{process}', lines, ctx.user)
+    print("setup the nginx...")
+    set_nginx(ctx, PROJECT_NAME, MY_DOMAIN_COM, PORT)
 
 
 @task
